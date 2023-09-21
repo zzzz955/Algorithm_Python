f80f116bed52560928bd9966726697d09a80b4dd
zzzz955 <zzzzz955@gmail.com>
2023-09-21 18:17:48 +0900

    ~q1655

---
+++ BaekJoon_Workbook2.py
@@ -0,0 +1,50 @@
+# 단기간 성장
+# https://www.acmicpc.net/workbook/view/4349
+
+
+def q12865():
+    # 평범한 배낭
+    n, k = map(int, input().split())
+    weights = []
+    values = []
+    for i in range(n):
+        w, v = map(int, input().split())
+        weights.append(w)
+        values.append(v)
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            if weights[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    print(dp[n][k])
+
+
+def q1655():
+    # 가운데를 말해요
+    import sys
+    import heapq
+    n = int(sys.stdin.readline())
+    result = []
+
+    max_heap = []
+    min_heap = []
+
+    for _ in range(n):
+        x = int(sys.stdin.readline())
+
+        if len(max_heap) == len(min_heap):
+            heapq.heappush(max_heap, (-x, x))
+        else:
+            heapq.heappush(min_heap, (x, x))
+
+        if min_heap and max_heap[0][1] > min_heap[0][0]:
+            min = heapq.heappop(min_heap)[0]
+            max = heapq.heappop(max_heap)[1]
+            heapq.heappush(max_heap, (-min, min))
+            heapq.heappush(min_heap, (max, max))
+        result.append(max_heap[0][1])
+    for data in result:
+        print(data)
+q1655()
