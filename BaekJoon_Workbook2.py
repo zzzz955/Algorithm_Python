07065f69a91f4eaac9e646a5f2d97f0b98e5c32a
zzzz955 <zzzzz955@gmail.com>
2023-10-04 12:10:03 +0900

    q1463 # 1로 만들기

--- BaekJoon_Workbook2.py
+++ BaekJoon_Workbook2.py
@@ -48,3 +48,16 @@ def q1655():
     for data in result:
         print(data)
 
+
+def q1463():
+    # 1로 만들기
+    x = int(input())
+    dp = [0] * (x + 1)
+    for i in range(2, x + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+    print(dp[x])
+
