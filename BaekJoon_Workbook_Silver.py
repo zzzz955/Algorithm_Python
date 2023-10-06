6d3add02ace7e968029148898c94b5cf9b70b48c
SangHyeok <zzzzz955@gmail.com>
2023-10-07 02:30:14 +0900

    def q9095():     # 1, 2, 3 더하기

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -291,5 +291,19 @@ def q2839():
             print(-1)
 
 
+def q9095():
+    # 1, 2, 3 더하기
+    t = int(input())
+    result = []
+    dp = [0] * 11
+    dp[1:4] = [1, 2, 4]
+    for i in range(4, 11):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    for _ in range(t):
+        n = int(input())
+        result.append(dp[n])
+    for answer in result:
+        print(answer)
+q9095()
 
 
