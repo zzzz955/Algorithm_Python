4611414b067870a1831fc8c034bbb5f610c8306c
zzzz955 <zzzzz955@gmail.com>
2023-10-10 15:19:00 +0900

    def q2609():     # 최대공약수와 최소공배수

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -1,3 +1,5 @@
+
+
 def q14563():
     # 완전수
     n = int(input())
@@ -47,4 +49,28 @@ def q2775():
         result.append(dp[k][n])
     for answer in result:
         print(answer)
-q2775()
+
+
+def q2609():
+    # 최대공약수와 최소공배수
+    n1, n2 = map(int, input().split())
+    for_lcm = n1 * n2
+    for_gcd = 0
+    if n1 >= n2:
+        while n2 != 0:
+            r = n1 % n2
+            n1 = n2
+            n2 = r
+        for_gcd = n1
+    else:
+        while n1 != 0:
+            r = n2 % n1
+            n2 = n1
+            n1 = r
+        for_gcd = n2
+    gcd = for_gcd
+    lcm = for_lcm // gcd
+    print(gcd)
+    print(lcm)
+q2609()
+
