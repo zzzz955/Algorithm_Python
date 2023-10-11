0f34cc99ba9e3988345d0b8fb65ceed55bbcf8b0
zzzz955 <zzzzz955@gmail.com>
2023-10-11 17:25:13 +0900

    def q11723():     # 집합

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def q1010():
     # 다리 놓기
     t = int(input())
@@ -209,7 +212,7 @@ def q4659():
             if password[i] in moeum and password[i + 1] in moeum and password[i + 2] in moeum:
                 test = False
                 break
-            if not(password[i] in moeum) and not(password[i + 1] in moeum) and not(password[i + 2] in moeum):
+            if not (password[i] in moeum) and not (password[i + 1] in moeum) and not (password[i + 2] in moeum):
                 test = False
                 break
         for i in range(len(password) - 1):
@@ -347,4 +350,34 @@ def q1929():
     for i in range(m, n + 1):
         if dp[i]:
             sys.stdout.write(str(i) + '\n')
-q1929()
+
+
+def q11723():
+    # 집합
+    import sys
+    m = int(sys.stdin.readline())
+    dp = [0] * 21
+    for _ in range(m):
+        val = sys.stdin.readline().strip()
+        if val == 'all':
+            dp = [1] * 21
+        elif val == 'empty':
+            dp = [0] * 21
+        else:
+            op, n = val.split()
+            n = int(n)
+            if op == 'add':
+                dp[n] = 1
+            elif op == 'remove':
+                dp[n] = 0
+            elif op == 'toggle':
+                if dp[n]:
+                    dp[n] = 0
+                else:
+                    dp[n] = 1
+            else:
+                if dp[n]:
+                    print(1)
+                else:
+                    print(0)
+q11723()
