0883e2dbf9112f78cbff549ad0fe5ba6f45f5edc
zzzz955 <zzzzz955@gmail.com>
2023-10-19 11:25:17 +0900

    def q1515():     # 수 이어 쓰기

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -694,4 +694,18 @@ def q21921():
         print(count)
 
 
-q21921()
+def q1515():
+    # 수 이어 쓰기
+    nums = input()
+    i = 0
+    while 1:
+        i += 1
+        num = str(i)
+        while len(num) > 0 and len(nums) > 0:
+            if num[0] == nums[0]:
+                nums = nums[1:]
+            num = num[1:]
+        if nums == '':
+            print(i)
+            break
+q1515()
