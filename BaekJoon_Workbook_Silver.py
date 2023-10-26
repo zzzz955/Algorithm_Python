62587d0cb55dcf30e921c65c38db96b5f55a706f
zzzz955 <zzzzz955@gmail.com>
2023-10-26 17:49:22 +0900

    def q1927():     # 최소 힙

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -902,4 +902,22 @@ def q22233():
             if sentence in lst:
                 lst.remove(sentence)
         print(len(lst))
-q22233()
+
+
+def q1927():
+    # 최소 힙
+    import sys
+    import heapq
+
+    n = int(sys.stdin.readline())
+    lst = []
+    for _ in range(n):
+        x = int(int(sys.stdin.readline()))
+        if x > 0:
+            heapq.heappush(lst, x)
+        else:
+            if lst:
+                print(heapq.heappop(lst))
+            else:
+                print(0)
+q1927()
