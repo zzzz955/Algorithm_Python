83aa0d97518de283f513f80e81d1c29d3a1d283a
SangHyeok <zzzzz955@gmail.com>
2023-11-09 17:08:23 +0900

    def q20006():     # 랭킹전 대기열

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -920,32 +920,32 @@ def q1927():
 
 
 def q20006():
-    # 랭킹전 대기열(재채점 필요)
+    # 랭킹전 대기열
     p, m = map(int, input().split())
-    room = {}
+    room = []
 
     for _ in range(p):
         l, n = input().split()
         l = int(l)
         do = False
-        for key in room.keys():
-            if key - 10 <= l <= key + 10 and len(room[key]) < 5:
-                room[key].append([l, n])
+        for key in room:
+            if key[0][0] - 10 <= l <= key[0][0] + 10 and len(key) < m:
+                key.append([l, n])
                 do = True
                 break
         if not do:
-            room[l] = []
-            room[l].append([l, n])
-    for key, item in room.items():
-        if len(item) == m:
-            item = sorted(item, key=lambda x: x[1])
+            room.append([])
+            room[-1].append([l, n])
+    for key in room:
+        if len(key) == m:
+            key = sorted(key, key=lambda x: x[1])
             print('Started!')
-            for i in item:
+            for i in key:
                 print(*i)
         else:
-            item = sorted(item, key=lambda x: x[1])
+            key = sorted(key, key=lambda x: x[1])
             print('Waiting!')
-            for i in item:
+            for i in key:
                 print(*i)
 q20006()
 
