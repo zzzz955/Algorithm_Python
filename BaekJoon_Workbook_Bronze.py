47cf21390a3086afd40c54686170fda368362b80
SangHyeok <zzzzz955@gmail.com>
2023-12-14 15:53:16 +0900

    def q24267():     # 알고리즘 수업 - 알고리즘의 수행 시간 6

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -823,5 +823,17 @@ def q24266():
     n = int(input())
     print(n * n * n)
     print(3)
-q24266()
+
+
+def q24267():
+    # 알고리즘 수업 - 알고리즘의 수행 시간 6
+    n = int(input())
+    result = 0
+    s = 0
+    for i in range(1, n - 1):
+        s += i
+        result += s
+    print(result)
+    print(3)
+q24267()
 
