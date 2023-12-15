51b95c160db96cb3fad752adba4e34b7d85059c4
SangHyeok <zzzzz955@gmail.com>
2023-12-15 13:53:15 +0900

    def q24313():     # 알고리즘 수업 - 점근적 표기 1

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -1135,4 +1135,15 @@ def q1193():
         first = dp[index + 1] - x + 1
         last = x - dp[index]
         print(str(first) + '/' + str(last))
-q1193()
+
+
+def q24313():
+    # 알고리즘 수업 - 점근적 표기 1
+    a1, a2 = map(int, input().split())
+    c = int(input())
+    n0 = int(input())
+    if a1 <= c and a1 * n0 + a2 <= c * n0:
+        print(1)
+    else:
+        print(0)
+q24313()
