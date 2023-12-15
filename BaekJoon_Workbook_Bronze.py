17b86a65a983320e839e7d8ba428f07c19d9b4b4
SangHyeok <zzzzz955@gmail.com>
2023-12-15 17:02:51 +0900

    def q19532():     # 수학은 비대면강의입니다

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -869,4 +869,13 @@ def q2231():
             result = i
             break
     print(result)
-q2231()
+
+
+def q19532():
+    # 수학은 비대면강의입니다
+    a, b, c, d, e, f = map(int, input().split())
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(x, y)
+q19532()
