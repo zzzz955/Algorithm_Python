e3a8fc2c0ec9d6d026a8fbf27329247879c3d08b
SangHyeok <zzzzz955@gmail.com>
2023-12-15 15:12:01 +0900

    def q2231():     # 분해합

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -848,4 +848,25 @@ def q2798():
                 if max_val < lst[i] + lst[j] + lst[k] <= m:
                     max_val = lst[i] + lst[j] + lst[k]
     print(max_val)
-q2798()
+
+
+def q2231():
+    # 분해합
+    n = int(input())
+    if n in [2, 4, 6, 8]:
+        print(n // 2)
+        return
+    if n > 19:
+        length = len(str(n)) * 9
+    else:
+        length = (len(str(n)) - 1) * 9
+    result = 0
+    for i in range(n - length, n):
+        temps = []
+        for j in str(i):
+            temps.append(int(j))
+        if i + sum(temps) == n:
+            result = i
+            break
+    print(result)
+q2231()
