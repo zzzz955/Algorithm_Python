853871b2f2ceff9a26674f58961e250a149993a7
SangHyeok <zzzzz955@gmail.com>
2024-01-18 02:53:56 +0900

    def q28279():     # 덱 2

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -1615,7 +1615,6 @@ def q4949():
             print('yes')
         else:
             print('no')
-q4949()
 
 
 def q12789():
@@ -1698,3 +1697,45 @@ def q11866():
     print('<', end='')
     print(', '.join(map(str, result)), end='')
     print('>')
+
+
+def q28279():
+    # 덱 2
+    import collections, sys
+
+    n = int(input())
+    q = collections.deque()
+    for _ in range(n):
+        order = sys.stdin.readline().split()
+        if order[0] == '1':
+            q.appendleft(order[1])
+        elif order[0] == '2':
+            q.append(order[1])
+        elif order[0] == '3':
+            if q:
+                print(q.popleft())
+            else:
+                print(-1)
+        elif order[0] == '4':
+            if q:
+                print(q.pop())
+            else:
+                print(-1)
+        elif order[0] == '5':
+            print(len(q))
+        elif order[0] == '6':
+            if q:
+                print(0)
+            else:
+                print(1)
+        elif order[0] == '7':
+            if q:
+                print(q[0])
+            else:
+                print(-1)
+        elif order[0] == '8':
+            if q:
+                print(q[-1])
+            else:
+                print(-1)
+q28279()
