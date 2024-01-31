c0f77a6a6ec8956f8d3e8861785416a7891c9b15
SangHyeok <zzzzz955@gmail.com>
2024-01-31 23:29:26 +0900

    def q25192():     # 인사성 밝은 곰곰이

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -1781,4 +1781,20 @@ def q24511():
             q.appendleft(num)
         else:
             print(num, end=' ')
-q24511()
+
+
+def q25192():
+    # 인사성 밝은 곰곰이
+    n = int(input())
+    dic = {}
+    result = 0
+    for _ in range(n):
+        s = input()
+        if s == 'ENTER':
+            result += len(dic)
+            dic.clear()
+        else:
+            dic[s] = 1
+    result += len(dic)
+    print(result)
+q25192()
