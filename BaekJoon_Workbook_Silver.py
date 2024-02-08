e2e2d551fceed45400951aa0abe9ebe1dc2644c3
SangHyeok <zzzzz955@gmail.com>
2024-02-08 16:18:39 +0900

    def q2579():     # 계단 오르기

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -1932,4 +1932,19 @@ def q15652():
             lst.pop()
 
     result(n, m)
-q15652()
+
+
+def q2579():
+    # 계단 오르기
+    n = int(input())
+    lst = [0] * 301
+    for i in range(1, n + 1):
+        lst[i] = int(input())
+    dp = [0] * 301
+    dp[1] = lst[1]
+    dp[2] = lst[1] + lst[2]
+    dp[3] = max(lst[1] + lst[3], lst[2] + lst[3])
+    for i in range(4, n + 1):
+        dp[i] = max(dp[i - 3] + lst[i - 1] + lst[i], dp[i - 2] + lst[i])
+    print(dp[n])
+q2579()
