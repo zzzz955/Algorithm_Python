f3557431c3386df5b160f06b3bdeb23c873a31e9
SangHyeok <zzzzz955@gmail.com>
2024-02-20 04:38:20 +0900

    def q1541():     # 잃어버린 괄호

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2043,5 +2043,32 @@ def q11399():
         current += num
         result += current
     print(result)
-q11399()
+
+
+def q1541():
+    # 잃어버린 괄호
+    s = input()
+    s += '+'
+    lst = []
+    index1 = 0
+    index2 = 0
+    minus = False
+    for i in range(len(s)):
+        if s[i] == '+':
+            index2 = i
+            if minus:
+                lst.append(-int(s[index1:index2]))
+            else:
+                lst.append(int(s[index1:index2]))
+            index1 = i + 1
+        elif s[i] == '-':
+            index2 = i
+            if minus:
+                lst.append(-int(s[index1:index2]))
+            else:
+                lst.append(int(s[index1:index2]))
+            index1 = i + 1
+            minus = True
+    print(sum(lst))
+q1541()
 
