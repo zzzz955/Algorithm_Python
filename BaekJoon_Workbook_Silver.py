af99933a713f93dc988698413094973119e2d0e9
SangHyeok <zzzzz955@gmail.com>
2024-02-22 23:37:09 +0900

    def q1654():     # 랜선 자르기

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2099,4 +2099,25 @@ def q1920():
             print(1)
         else:
             print(0)
-q1920()
+
+
+def q1654():
+    # 랜선 자르기
+    import sys
+
+    k, n = map(int, input().split())
+    lst = []
+    for _ in range(k):
+        lst.append(int(sys.stdin.readline()))
+    start, end = 1, max(lst)
+    while start <= end:
+        mid = (start + end) // 2
+        lan = 0
+        for num in lst:
+            lan += num // mid
+        if lan >= n:
+            start = mid + 1
+        else:
+            end = mid - 1
+    print(end)
+q1654()
