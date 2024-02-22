55786bb5e2e4331a660f2922a9827ab609ca1bd4
SangHyeok <zzzzz955@gmail.com>
2024-02-22 23:46:26 +0900

    def q2805():     # 나무 자르기

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2120,4 +2120,22 @@ def q1654():
         else:
             end = mid - 1
     print(end)
-q1654()
+
+
+def q2805():
+    # 나무 자르기
+    n, m = map(int, input().split())
+    lst = list(map(int, input().split()))
+    start, end = 1, max(lst)
+    while start <= end:
+        mid = (start + end) // 2
+        height = 0
+        for num in lst:
+            if num > mid:
+                height += num - mid
+        if height >= m:
+            start = mid + 1
+        else:
+            end = mid - 1
+    print(end)
+q2805()
