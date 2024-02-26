42bcf10ea476506079052498d9c342778823ac93
SangHyeok <zzzzz955@gmail.com>
2024-02-26 22:52:47 +0900

    def q13301():     # 타일 장식물

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2193,4 +2193,17 @@ def q10826():
     for i in range(2, n + 1):
         dp[i] = dp[i - 2] + dp[i - 1]
     print(dp[n])
-q10826()
+
+
+def q13301():
+    # 타일 장식물
+    n = int(input())
+    dp = [1] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    dp2 = [4] * (n + 1)
+    for i in range(2, n + 1):
+        dp2[i] = dp2[i - 1] + (dp[i - 1] * 2)
+    print(dp2[n])
+q13301()
