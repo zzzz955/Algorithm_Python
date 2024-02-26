bb6341b2fc13d804528c62525fca0628d268fdee
SangHyeok <zzzzz955@gmail.com>
2024-02-26 22:30:12 +0900

    def q10826():     # 피보나치 수 4

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2183,4 +2183,14 @@ def q9625():
     for i in range(2, 46):
         dp[i] = dp[i - 2] + dp[i - 1]
     print(dp[k - 1], dp[k])
-q9625()
+
+
+def q10826():
+    # 피보나치 수 4
+    n = int(input())
+    dp = [0] * 10001
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    print(dp[n])
+q10826()
