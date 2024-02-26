10ed998f6d6c2e5fdf4d7352574163cf5b6bc3d7
SangHyeok <zzzzz955@gmail.com>
2024-02-26 21:10:31 +0900

    def q11660():     # 구간 합 구하기 5

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2138,4 +2138,19 @@ def q2805():
         else:
             end = mid - 1
     print(end)
-q2805()
+
+
+def q11660():
+    # 구간 합 구하기 5
+    import sys
+
+    n, m = map(int, input().split())
+    lst = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + lst[i - 1][j - 1]
+    for _ in range(m):
+        x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+        print(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1])
+q11660()
