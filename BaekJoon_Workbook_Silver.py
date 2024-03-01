87918dc9b2282e01a8ba513d4e67f1516b259d84
SangHyeok <zzzzz955@gmail.com>
2024-03-01 18:42:11 +0900

    def q10211():     # Maximum Subarray

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2331,4 +2331,19 @@ def q2670():
         dp[i] = max(dp[i - 1] * lst[i - 1], lst[i - 1])
     dp[0] = 0
     print(f'{max(dp):.3f}')
-q2670()
+
+
+def q10211():
+    # Maximum Subarray
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        lst = list(map(int, input().split()))
+        dp = [0] * (n + 1)
+        if max(lst) < 0:
+            print(max(lst))
+            continue
+        for i in range(1, n + 1):
+            dp[i] = max(dp[i - 1] + lst[i - 1], 0)
+        print(max(dp))
+q10211()
