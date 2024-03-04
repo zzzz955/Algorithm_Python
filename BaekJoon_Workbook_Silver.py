b9a8f49cd437857b522db45e5dcb82492399c277
SangHyeok <zzzzz955@gmail.com>
2024-03-04 21:51:23 +0900

    def q15624():     # 피보나치 수 7

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2357,4 +2357,14 @@ def q13699():
         for j in range(i):
             dp[i] += dp[j] * dp[i - j - 1]
     print(dp[n])
-q13699()
+
+
+def q15624():
+    # 피보나치 수 7
+    n = int(input())
+    dp = [0] * 1000001
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007
+    print(dp[n])
+q15624()
