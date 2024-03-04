ff4bb9ea11b97bea88a3f589bd5088c72bdd0092
SangHyeok <zzzzz955@gmail.com>
2024-03-04 22:00:35 +0900

    def q14495():     # 피보나치 비스무리한 수열

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2367,4 +2367,14 @@ def q15624():
     for i in range(2, n + 1):
         dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007
     print(dp[n])
-q15624()
+
+
+def q14495():
+    # 피보나치 비스무리한 수열
+    n = int(input())
+    dp = [0] * 117
+    dp[1] = dp[2] = dp[3] = 1
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 3] + dp[i - 1]
+    print(dp[n])
+q14495()
