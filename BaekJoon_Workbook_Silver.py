91c345e2eace7b6972a4e9d302bdf70a00f26672
SangHyeok <zzzzz955@gmail.com>
2024-03-04 21:43:04 +0900

    def q13699():     # 점화식

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -2346,4 +2346,15 @@ def q10211():
         for i in range(1, n + 1):
             dp[i] = max(dp[i - 1] + lst[i - 1], 0)
         print(max(dp))
-q10211()
+
+
+def q13699():
+    # 점화식
+    n = int(input())
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i - j - 1]
+    print(dp[n])
+q13699()
