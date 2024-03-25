9dc0d949ae44844c9fc4ff2ae21968ab2f2bc1dc
SangHyeok <zzzzz955@gmail.com>
2024-03-25 23:01:26 +0900

    def q2358():     # 평행선

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -3009,4 +3009,31 @@ def q1835():
         for j in range(i):
             deq.appendleft(deq.pop())
     print(*deq)
-q1835()
+
+
+def q2358():
+    # 평행선
+    import sys
+
+    n = int(sys.stdin.readline())
+    dic_x = {}
+    dic_y = {}
+    for _ in range(n):
+        x, y = map(int, sys.stdin.readline().split())
+        if x in dic_x:
+            dic_x[x] = True
+        else:
+            dic_x[x] = False
+        if y in dic_y:
+            dic_y[y] = True
+        else:
+            dic_y[y] = False
+    result = 0
+    for val in dic_x.values():
+        if val:
+            result += 1
+    for val in dic_y.values():
+        if val:
+            result += 1
+    print(result)
+q2358()
