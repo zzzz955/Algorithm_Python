f651d3aa1729c484375f40e147450430e57bd546
SangHyeok <zzzzz955@gmail.com>
2024-04-02 19:18:07 +0900

    def q22864():     # 피로도

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -1383,4 +1383,20 @@ def q14487():
     lst = list(map(int, input().split()))
     lst.remove(max(lst))
     print(sum(lst))
-q14487()
+
+
+def q22864():
+    # 피로도
+    a, b, c, m = map(int, input().split())
+    result = 0
+    tired = 0
+    for _ in range(24):
+        if tired + a <= m:
+            tired += a
+            result += b
+        else:
+            tired -= c
+            if tired < 0:
+                tired = 0
+    print(result)
+q22864()
