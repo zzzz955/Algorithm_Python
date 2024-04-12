9536421482d4275c29023e882a5f696e7c2d313c
SangHyeok <zzzzz955@gmail.com>
2024-04-12 20:00:54 +0900

    def q27951():     # 옷걸이

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -1823,18 +1823,18 @@ def q9329():
 
 
 def q27951():
-    # 옷걸이(틀렸습니다로 재채점 필요)
+    # 옷걸이
     n = int(input())
     lst = list(map(int, input().split()))
     u, d = map(int, input().split())
     for i in lst:
-        if u < 0 or d < 0:
-            print('NO')
-            return
         if i == 1:
             u -= 1
         if i == 2:
             d -= 1
+    if u < 0 or d < 0:
+        print('NO')
+        return
     result = ''
     for i in lst:
         if i == 3:
