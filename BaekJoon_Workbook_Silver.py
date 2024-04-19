3419b5745d184028b60ef0be8ae9309bd432329b
SangHyeok <zzzzz955@gmail.com>
2024-04-19 17:25:05 +0900

    def q12033():     # 백준 12033번 파이썬 김인천씨의 식료품가게 (Small)

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -3671,4 +3671,20 @@ def q27940():
             break
     if not chk:
         print(-1)
-q27940()
+
+
+def q12033():
+    # 백준 12033번 파이썬 김인천씨의 식료품가게 (Small)
+    t = int(input())
+    case = 0
+    for _ in range(t):
+        case += 1
+        n = int(input())
+        lst = list(map(int, input().split()))
+        result = []
+        for i in lst:
+            if int(i * (4 / 3)) in lst:
+                lst.remove(int(i * (4 / 3)))
+                result.append(str(i))
+        print(f'Case #{case}: {" ".join(result)}')
+q12033()
