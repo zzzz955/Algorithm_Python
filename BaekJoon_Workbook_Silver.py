23662ecee076f4199896a497764f50df83c82701
SangHyeok <zzzzz955@gmail.com>
2024-04-22 18:05:55 +0900

    # 백준 1946번 파이썬 신입 사원

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -3722,4 +3722,22 @@ def q28136():
     if lst[0] <= lst[-1]:
         result += 1
     print(result)
-q28136()
+
+
+def q1946():
+    # 백준 1946번 파이썬 신입 사원
+    import sys
+
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        n = int(sys.stdin.readline())
+        lst = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+        lst.sort(key=lambda x: (x[0]))
+        result = 0
+        top = lst[0][1]
+        for i in lst:
+            if top >= i[1]:
+                top = i[1]
+                result += 1
+        print(result)
+q1946()
