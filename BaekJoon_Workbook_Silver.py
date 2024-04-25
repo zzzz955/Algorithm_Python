d73d088a06563d7bcc9e8410a9b0e8651635a443
SangHyeok <zzzzz955@gmail.com>
2024-04-26 03:51:39 +0900

    백준 2606번 파이썬 바이러스

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -3902,4 +3902,25 @@ def q18111():
         if block >= 0 and sec[i] <= sec[h]:
             h = i
     print(sec[h], h)
-q18111()
+
+
+def q2606():
+    # 백준 2606번 파이썬 바이러스
+    n = int(input())
+    v = int(input())
+    graph = [[] for i in range(n + 1)]
+    visited = [0] * (n + 1)
+    for i in range(v):
+        a, b = map(int, input().split())
+        graph[a] += [b]
+        graph[b] += [a]
+
+    def dfs(v):
+        visited[v] = 1
+        for nx in graph[v]:
+            if visited[nx] == 0:
+                dfs(nx)
+
+    dfs(1)
+    print(sum(visited) - 1)
+q2606()
