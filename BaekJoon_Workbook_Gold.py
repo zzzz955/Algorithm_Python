0124029c76a6a50fa04c19602a2c580e4deb5c1b
Sang <zzzzz955@gmail.com>
2024-04-30 14:42:04 +0900

    백준 5430번 파이썬 AC

--- BaekJoon_Workbook_Gold.py
+++ BaekJoon_Workbook_Gold.py
@@ -152,4 +152,36 @@ def q12904():
         else:
             t = t[:-1][::-1]
     print(1 if s == t else 0)
-q12904()
+
+
+def q5430():
+    # 백준 5430번 파이썬 AC
+    import collections
+
+    t = int(input())
+    for _ in range(t):
+        p = input()
+        n = int(input())
+        s = input()[1:-1]
+        deq = collections.deque(s.split(','))
+        chk = True
+        rev = False
+        for char in p:
+            if char == 'R':
+                if rev:
+                    rev = False
+                else:
+                    rev = True
+            else:
+                if len(deq) >= 1 and not (len(deq) == 1 and deq[0] == ''):
+                    if rev:
+                        deq.pop()
+                    else:
+                        deq.popleft()
+                else:
+                    chk = False
+                    break
+        if rev:
+            deq.reverse()
+        print('[' + ','.join(deq) + ']' if chk else 'error')
+q5430()
