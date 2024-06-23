101abacff0ac2ffaf87e99d6cbbf8241ec832939
Sang <zzzzz955@gmail.com>
2024-06-23 18:31:36 +0900

    백준 1781번 파이썬 컵라면

--- BaekJoon_Workbook_Gold.py
+++ BaekJoon_Workbook_Gold.py
@@ -219,4 +219,23 @@ def q1727():
             if i < j:
                 dp[i][j] = min(dp[i][j], dp[i][j - 1])
     print(dp[n][m])
-q1727()
+
+
+def q1781():
+    # 백준 1781번 파이썬 컵라면
+    import sys, heapq
+
+    n = int(sys.stdin.readline())
+    lst = []
+    for _ in range(n):
+        d, c = map(int, sys.stdin.readline().split())
+        lst.append([d, c])
+    lst.sort(key=lambda x: (x[0], -x[1]))
+
+    stack = []
+    for i in lst:
+        heapq.heappush(stack, i[1])
+        if len(stack) > i[0]:
+            heapq.heappop(stack)
+    print(sum(stack))
+q1781()
