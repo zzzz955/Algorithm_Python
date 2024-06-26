2ed43bc9debd6c9304c22d08c37e011240d858fc
Sang <zzzzz955@gmail.com>
2024-06-26 16:05:31 +0900

    SWEA 1961번 D2 숫자 배열 회전 파이썬

--- SWEA_Problem.py
+++ SWEA_Problem.py
@@ -42,4 +42,31 @@ def q1959():
                     temp += a[j + k] * b[k]
             result = max(result, temp)
         print(f"#{i} {result}")
-q1959()
+
+
+def q1961():
+    # SWEA 1961번 D2 숫자 배열 회전 파이썬
+    t = int(input())
+    for i in range(1, t + 1):
+        n = int(input())
+        array = ''
+        for _ in range(n):
+            lst = list(map(str, input().split()))
+            for temp in lst:
+                for char in temp:
+                    array += char
+        angle90 = []
+        angle180 = []
+        angle270 = []
+        for j in range(n):
+            for k in range(n - 1, -1, -1):
+                angle90.append(array[j + k * n])
+        for j in range(len(array) - 1, -1, -1):
+            angle180.append(array[j])
+        for j in range(n - 1, -1, -1):
+            for k in range(n):
+                angle270.append(array[j + k * n])
+        print(f'#{i}')
+        for j in range(n):
+            print(''.join(angle90[j * n:j * n + n]), ''.join(angle180[j * n:j * n + n]), ''.join(angle270[j * n:j * n + n]))
+q1961()
