86d1b0de1f88b242cc2499edcd29c2c96b3506a3
Sang <zzzzz955@gmail.com>
2024-06-28 03:33:21 +0900

    백준 10158번 개미 파이썬

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -4902,5 +4902,22 @@ def q2477():
             if length[i] == big_h:
                 small_w = abs(length[(i + 1) % 6] - length[(i + 5) % 6])
     print(k * ((big_w * big_h) - (small_w * small_h)))
-q2477()
+
+
+def q10158():
+    # 백준 10158번 개미 파이썬
+    c, r = map(int, input().split())
+    p, q = map(int, input().split())
+    t = int(input())
+
+    x = (p + t) % (2 * c)
+    y = (q + t) % (2 * r)
+
+    if x > c:
+        x = 2 * c - x
+    if y > r:
+        y = 2 * r - y
+
+    print(x, y)
+q10158()
 
