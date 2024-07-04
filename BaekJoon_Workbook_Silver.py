bc6bef5297b36c063b96dbe279d376c22f3ae530
Sang <zzzzz955@gmail.com>
2024-07-05 01:23:39 +0900

    백준 4673번 셀프 넘버 파이썬

--- BaekJoon_Workbook_Silver.py
+++ BaekJoon_Workbook_Silver.py
@@ -5001,7 +5001,20 @@ def q3085():
                     print(n)
                     return
     print(result)
-q19947()
 
 
+def q4673():
+    # 백준 4673번 셀프 넘버 파이썬
+    dp = [False] * 10001
+    for i in range(1, 10001):
+        temp = i
+        while i > 0:
+            temp += i % 10
+            i //= 10
+        if temp < 10000:
+            dp[temp] = True
+    for i in range(1, 10000):
+        if not dp[i]:
+            print(i)
+q4673()
 
