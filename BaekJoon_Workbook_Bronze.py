5be12d719e4cc4ae9016c920e5c65b11702a42db
Sang <zzzzz955@gmail.com>
2024-07-06 21:09:58 +0900

    백준 2445번 별 찍기 - 8 파이썬

--- BaekJoon_Workbook_Bronze.py
+++ BaekJoon_Workbook_Bronze.py
@@ -2170,4 +2170,13 @@ def q2443():
     n = int(input())
     for i in range(n):
         print(' ' * i + '*' * (2 * n - (2 * i + 1)))
-q2443()
+
+
+def q2445():
+    # 백준 2445번 별 찍기 - 8 파이썬
+    n = int(input())
+    for i in range(1, n + 1):
+        print('*' * i + ' ' * (n - i) * 2 + '*' * i)
+    for i in range(1, n):
+        print('*' * (n - i) + ' ' * i * 2 + '*' * (n - i))
+q2445()
